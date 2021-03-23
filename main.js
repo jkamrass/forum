c99ea87554229e090595c133fbb592c32b8c2b1d
unknown <joshkamrass@gmail.com>
2021-03-23 12:52:45 -0400

    completes post button functionality

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// your code here
+var posts = document.getElementsByClassName('posts')[0];
+var nameInput = document.getElementById('name');
+var messageInput = document.getElementById('message');
+var button = document.getElementById('submit');
+
+button.addEventListener('click', () => {
+  var postP = document.createElement('p');
+  var authorP = document.createElement('p');
+  authorP.innerHTML = 'Posted By: <strong>' + nameInput.value + '</strong>';
+  postP.innerHTML = messageInput.value;
+  posts.append(postP);
+  posts.append(authorP);
+})
